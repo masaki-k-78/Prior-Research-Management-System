07c11db811f305b7a2b6e1fe93483403d71fc96c
masaki-k-78 <masaki120708koko@gmail.com>
2023-12-12 17:48:22 +0900

    make __main__

---
+++ __main__.py
@@ -0,0 +1,9 @@
+from PRMS.add_Prior_Research import Add_Prior_Research
+from PRMS.main_System import Main_System
+
+def main():
+    print("Welcome!")
+    MS = Main_System()
+
+if __name__ == '__main__':
+    main()
