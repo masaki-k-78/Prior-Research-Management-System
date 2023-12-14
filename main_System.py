9ca4d00e8fbc1db3a574c414324f1766d3b1de96
masaki-k-78 <masaki120708koko@gmail.com>
2023-12-15 04:21:24 +0900

    separate __init__ and ar(). Next, search system

--- main_System.py
+++ main_System.py
@@ -1,13 +1,26 @@
 from PRMS.add_Prior_Research import Add_Prior_Research
 
+#システムの基本的な動作を担う
 class Main_System():
     def __init__(self):
-        print("What do you do?")
-        print("1: add research, 2: coming soon...")
+        print("--What do you do?")
+        print("1: add research, 2: search research", "3: coming soon...")
         user_input = input()
 
         if user_input == "1":
-            APR = Add_Prior_Research()
-            APR.add_title(input("Title: "))
+            self.ar()
+        elif user_input == "2":
+            self.sr()
         else:
             print("Undefined number.")
+
+    def ar(self):
+        PRList = []
+        APR = Add_Prior_Research()
+        #論文の基本情報を入力
+        APR.add_basic_info(PRList)
+        #ファイルに書き込み
+        APR.write_basic_info(PRList)
+
+    def sr(self):
+        print("未実装")
