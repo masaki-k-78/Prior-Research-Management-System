5e4b2e851a96e5eee660e6d062f7abb7be683d89
masaki-k-78 <masaki120708koko@gmail.com>
2023-12-15 04:22:33 +0900

    add add_basic_info() and write_basic_info().

--- add_Prior_Research.py
+++ add_Prior_Research.py
@@ -1,6 +1,23 @@
+import csv
 
+#論文の基本情報を追加するための機能を担う
 class Add_Prior_Research():
+
     def __init__(self):
-        print("OK")
-    def add_title(self, title):
-        print("Title: " + title)
+        print("--OK")
+
+    def add_basic_info(self, PRList):
+        PRList.append(input("Title: "))
+        PRList.append(input("Author: "))
+        PRList.append(input("Conference: "))
+        PRList.append(input("Date: "))
+
+    def write_basic_info(self, PRList):
+        csv_path = "Research_Data.csv"
+
+        with open(csv_path, 'a', newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(PRList)
+        f.close()
+
+        print("--Finished add basic info.")
