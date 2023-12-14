0826040945026ac68f0673441ea022cf5b8c3d31
masaki-k-78 <masaki120708koko@gmail.com>
2023-12-15 04:19:21 +0900

    add while loop to exit or continue with the system

--- __main__.py
+++ __main__.py
@@ -2,8 +2,18 @@ from PRMS.add_Prior_Research import Add_Prior_Research
 from PRMS.main_System import Main_System
 
 def main():
-    print("Welcome!")
-    MS = Main_System()
+    print("--Welcome!")
+    input_corq = input("--Press c to continue or q to stop.: ")
+    
+    if input_corq == "c" or input_corq == "q":
+        while(input_corq != "q"):
+            MS = Main_System()
+            input_corq = input("--Press c to continue or q to stop.: ")
+        if input_corq == "q":
+            print("--PRMS ended.")
+    else:
+        print("--Undefined alphabet. PRMS ended.")
+
 
 if __name__ == '__main__':
     main()
