c9c7f14cf54efac2dcd96a2210b68c29d5020e47
masaki-k-78 <masaki120708koko@gmail.com>
2023-12-26 21:54:19 +0900

    change startup behavior

--- __main__.py
+++ __main__.py
@@ -3,16 +3,16 @@ from PRMS.main_System import Main_System
 
 def main():
     print("--Welcome!")
-    input_corq = input("--Press c to continue or q to stop.: ")
-    
-    if input_corq == "c" or input_corq == "q":
-        while(input_corq != "q"):
-            MS = Main_System()
-            input_corq = input("--Press c to continue or q to stop.: ")
+
+    #起動時以外は，続けるかどうかをユーザーに尋ねる．
+    input_corq = "c"
+    while(input_corq == "c"):
+        MS = Main_System()
+        input_corq = input("--Press c to continue or q to stop: ")
         if input_corq == "q":
             print("--PRMS ended.")
-    else:
-        print("--Undefined alphabet. PRMS ended.")
+        elif input_corq != "c":
+            print("--Undefined alphabet. PRMS ended.")
 
 
 if __name__ == '__main__':
