cd4c8c7c0d4da71907b5f3c11206a8b2011d12da
masaki-k-78 <masaki120708koko@gmail.com>
2023-12-30 04:20:17 +0900

    add ~_to_mysql()

--- add_Prior_Research.py
+++ add_Prior_Research.py
@@ -1,4 +1,5 @@
 import csv
+from PRMS.functions_Mysql import Functions_MySQL
 
 #論文の基本情報を追加するための機能を担う
 class Add_Prior_Research():
@@ -13,7 +14,7 @@ class Add_Prior_Research():
         PRList.append(input("Date: "))
 
     def write_basic_info(self, PRList):
-        csv_path = "Research_Data.csv"
+        csv_path = "PRMS/Research_Data.csv"
 
         with open(csv_path, 'a', newline="") as f:
             writer = csv.writer(f)
@@ -21,3 +22,22 @@ class Add_Prior_Research():
         f.close()
 
         print("--Finished add basic info.")
+
+    #以下，MySQLを用いた実装
+
+    def add_basic_info_to_mysql(self, PRList):
+        pr_title = input("Title: ")
+        pr_author = input("Author: ")
+        pr_conference = input("Conferene: ")
+        pr_date = input("Date: ")
+        PRList.append((pr_title, pr_author, pr_conference, pr_date))
+        print(PRList)
+
+    def write_basic_info_to_mysql(self, PRList, connection):
+        sql = '''
+        INSERT INTO mine (PR_title, PR_author, PR_conference, PR_date) 
+        VALUES (%s, %s, %s, %s)
+        '''
+
+        FM = Functions_MySQL()
+        FM.execute_list_query(connection, sql, PRList)
